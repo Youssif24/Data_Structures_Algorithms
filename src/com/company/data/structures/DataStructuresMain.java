e46caf2b767d22e4afd091c478edc39047ca29ea
Youssif24 <youssif.saad96@gmail.com>
2020-09-26 17:09:58 +0200

    Array Implementation(Simple - objects - two dim)

--- src/com/company/data/structures/DataStructuresMain.java
+++
@@ -1,9 +0,0 @@
-package com.company.data.structures;
-
-public class DataStructuresMain {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("This is data structures project");
-    }
-}
